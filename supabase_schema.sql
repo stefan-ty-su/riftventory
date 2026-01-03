dc44f89ca280e73308cd5e112d65436735fafb75
stefan-ty-su <stefansu3526@gmail.com>
2026-01-03 16:10:38 +1100

    Updated supabase schema

--- supabase_schema.sql
+++ supabase_schema.sql
@@ -2,7 +2,6 @@
 -- Table order and constraints may not be valid for execution.
 
 CREATE TABLE public.card (
-  id text NOT NULL,
   set_id text NOT NULL,
   card_number smallint NOT NULL,
   card_id text NOT NULL UNIQUE,
@@ -24,10 +23,28 @@ CREATE TABLE public.card (
   alternate_art boolean DEFAULT false,
   overnumbered boolean DEFAULT false,
   signature boolean DEFAULT false,
-  CONSTRAINT card_pkey PRIMARY KEY (id),
+  CONSTRAINT card_pkey PRIMARY KEY (card_id),
   CONSTRAINT card_set_id_fkey FOREIGN KEY (set_id) REFERENCES public.set(set_id)
 );
-
+CREATE TABLE public.inventory (
+  inventory_id uuid NOT NULL DEFAULT gen_random_uuid(),
+  user_id text NOT NULL,
+  inventory_name text DEFAULT 'My Inventory'::text,
+  inventory_colour text,
+  created_at timestamp with time zone,
+  last_updated timestamp with time zone,
+  CONSTRAINT inventory_pkey PRIMARY KEY (inventory_id),
+  CONSTRAINT inventory_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.user(user_id)
+);
+CREATE TABLE public.inventory_card (
+  inventory_id uuid NOT NULL DEFAULT gen_random_uuid(),
+  card_id text NOT NULL,
+  quantity smallint DEFAULT '0'::smallint,
+  is_tradeable boolean DEFAULT false,
+  CONSTRAINT inventory_card_pkey PRIMARY KEY (inventory_id, card_id),
+  CONSTRAINT inventory_card_inventory_id_fkey FOREIGN KEY (inventory_id) REFERENCES public.inventory(inventory_id),
+  CONSTRAINT inventory_card_card_id_fkey FOREIGN KEY (card_id) REFERENCES public.card(card_id)
+);
 CREATE TABLE public.set (
   id text NOT NULL,
   set_name text NOT NULL,
@@ -36,4 +53,10 @@ CREATE TABLE public.set (
   card_count smallint,
   set_publish_date timestamp without time zone,
   CONSTRAINT set_pkey PRIMARY KEY (id)
+);
+CREATE TABLE public.user (
+  user_id text NOT NULL,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  user_name text NOT NULL UNIQUE,
+  CONSTRAINT user_pkey PRIMARY KEY (user_id)
 );
